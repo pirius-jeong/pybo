cb6f4f85e311e6d2be04cfcf845e33ca6888cd53
정영필 <pirius75@gmail.com>
2022-01-16 09:09:21 +0900

    postgres

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,13 @@
 from .base import *
 
 ALLOWED_HOSTS = ['192.168.137.101']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '192.168.137.101',
+        'PORT': '5432',
+    }
+}
